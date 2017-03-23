f255664f2e372450c4aeb0804d39e85683f26a2a
jay <doudoujay@qq.com>
2017-03-23 15:54:01 -0400

    init Parser&ParserException

---
+++ src/ParseException.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jay on 2017/3/23.
+ */
+public class ParseException extends Exception {
+}
---
+++ src/Parser.java
@@ -0,0 +1,25 @@
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ * Created by jay on 2017/3/23.
+ */
+public class Parser {
+    public Parser() {
+    }
+
+    public Document getDocument(String url)
+            throws ParseException {
+        return null;
+    }
+
+    public Elements getLinks(Document doc)
+            throws ParseException {
+        return null;
+    }
+
+    public String getBody(Document doc)
+            throws ParseException {
+        return null;
+    }
+}
