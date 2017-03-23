e240477cfd490e3ee298e6afc36a8e6b8c308267
hsu82 <hsu82@purdue.edu>
2017-03-22 22:09:13 -0400

    Update MyQueue.java

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -2,8 +2,43 @@
  * Created by jay on 2017/3/21.
  */
 public class MyQueue {
-    Node head;
-    public Node add() {}
-    public Node remove() {}
-    public boolean isEmpty() {}
+    private Node head;
+    private int count; //Queue size
+    private Object monitor;
+    ArrayList<Node> queue;
+
+    public MyQueue(){
+        if(!isEmpty()) {
+            queue.add(head);
+        }
+        count = 0;
+
+    }
+
+
+
+    public Node add() {
+        Node next = head.getNext();
+        while(next.getNext() != null) {
+            next = next.getNext();
+        }
+        queue.add(next);
+        count++;
+        return next;
+    }
+
+    public Node remove() { //remove first element in Queue
+        this.head = null;
+        count--;
+    }
+    public boolean isEmpty() {
+        if(count = 0 || head = null){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return count;
+    }
 }
