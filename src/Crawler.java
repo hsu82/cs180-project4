ca0723361999e55e32a3a606c7b429bd59d0b212
jay <doudoujay@qq.com>
2017-03-24 20:18:21 -0400

    fixed parser. init crawler

---
+++ src/Crawler.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jay on 2017/3/24.
+ */
+public class Crawler {
+    public Crawler() {
+
+    }
+}
--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,4 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -13,27 +14,41 @@ public class Parser {
 
     public Document getDocument(String url)
             throws ParseException {
-        if (url == null){
+        if (url == null) {
             throw new ParseException("getDocument() failed. String url is null.");
         }
-        if(url.equals("")){
+        if (url.equals("")) {
             throw new ParseException("getDocument() failed. String url is empty.");
         }
         try {
-            Document d = Jsoup.connect(url).get();
-            if(d == null){
+            Connection c = Jsoup.connect(url).timeout(3000);
+            Document d = null;
+            try {
+                d = c.get();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+            if (d == null) {
                 throw new ParseException("getDocument() failed: Document is null.");
             }
             return d;
-        } catch (IOException e) {
+        }catch (IllegalArgumentException e){
             e.printStackTrace();
+            throw new ParseException("getDocument() failed. Connection failed.");
+
         }
-        return null;
+
+
+
+
+
+
     }
 
     public Elements getLinks(Document doc)
             throws ParseException {
-        if(doc == null){
+        if (doc == null) {
             throw new ParseException("getLinks() failed. Document parameter is null.");
         }
         Elements links = doc.select("a[href]");
@@ -42,7 +57,7 @@ public class Parser {
 
     public String getBody(Document doc)
             throws ParseException {
-        if (doc == null){
+        if (doc == null) {
             throw new ParseException("getBody() failed. Document parameter is null.");
         }
         return doc.body().text();
